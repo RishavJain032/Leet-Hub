9b4e3a1cef1fd44ee16616c63c5a509062f17629
Rishav Jain <100018302+RishavPaharia@users.noreply.github.com>
2022-09-26 12:30:09 +0530

    Time: 25 ms (66.48%), Space: 66.5 MB (72.85%) - LeetHub

--- 456-132-pattern/456-132-pattern.java
+++ 456-132-pattern/456-132-pattern.java
@@ -27,7 +27,7 @@ class Solution {
 //             return false;
 //         }
 
-//         for (int i = 1; i < n - 2; i++) {
+//         for (int i = 0; i < n - 2; i++) {
 //             for (int j = i + 1; j < n - 1; j++) {
 //                 for (int k = j + 1; k < n; k++) {
 //                     if (arr[i] < arr[k] && arr[k] < arr[j]) {
