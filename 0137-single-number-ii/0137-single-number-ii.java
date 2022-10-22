f6a639322a42b10f178dcd6b061b40c7ff7be0f6
Rishav Jain <100018302+RishavJain032@users.noreply.github.com>
2022-10-22 19:08:45 +0530

    Time: 18 ms (12.03%), Space: 43.9 MB (57.68%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int singleNumber(int[] arr) {
+        int n = arr.length;
+        Arrays.sort(arr);
+        if(n > 1 && arr[0] != arr[1]){
+            return arr[0];
+        }
+        if(n > 1 && arr[n - 1] != arr[n - 2]) {
+            return arr[n - 1];
+        }
+        for(int i = 1; i < n - 1; i++){
+            if(arr[i] != arr[i - 1] && arr[i] != arr[i + 1]){
+                return arr[i];
+            }
+        }
+        return 1;
+    }
+}
